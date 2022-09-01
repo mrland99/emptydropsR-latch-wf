111c53c3de04f9aaf3c9b39c610a941d9d089ed3
mrland99 <max.ruikang.land@gmail.com>
2022-08-31 23:23:19 -0400

    add test data

--- wf/__init__.py
+++ wf/__init__.py
@@ -5,6 +5,7 @@ Distinguishing cells from empty droplets in droplet-based single-cell RNA sequen
 import subprocess
 from latch import small_task, workflow
 from latch.types import LatchFile, LatchDir
+from latch.resources.launch_plan import LaunchPlan
 
 @small_task
 def execute_Rscript(
@@ -40,13 +41,23 @@ def run_emptydrops(
 
     EmptyDrops
     ----
+    ### Summary
+    EmptydropsR is a statistical method that distinguishes between droplets containing cells and ambient RNA in a droplet-based single-cell RNA sequencing experiment. It identifies real cells by detecting significant deviations from the expression profile of the ambient solution.
 
-    Distinguish between droplets containing cells and ambient RNA in a droplet-based single-cell RNA sequencing experiment.
+    ### Input
+    This version of the workflow is designed to take 10X's raw feature `matrix.mtx` as input. In other words, the input is a `.mtx` file of genes (rows) x cells (cols).
 
-    The manuscript can be found [here](https://genomebiology.biomedcentral.com/articles/10.1186/s13059-019-1662-y)
+    ### Output
+    This workflow outputs two files:
+    - `metadata.h5:` stores values of arguments (inputs to emptydropsR) during run time.
+    - `output.h5:` returns a `LogProb`, `PValue`, and `FDR` that should be used by the user to filter out emptydrops.
+      
+    More information can be found here: https://rdrr.io/github/MarioniLab/DropletUtils/man/emptyDrops.html
 
-    Other Notes:
-      - The input is a `.mtx` file of genes (rows) x cells (cols).
+    ### Additional References
+    The manuscript can be found here: https://genomebiology.biomedcentral.com/articles/10.1186/s13059-019-1662-y
+
+    The source code can be found here: https://github.com/MarioniLab/DropletUtils
 
     __metadata__:
         display_name: emptyDropsR
@@ -97,4 +108,15 @@ def run_emptydrops(
         out_dir = out_dir,
         lower = lower, 
         ignore = ignore
+)
+
+LaunchPlan(
+    run_emptydrops,
+    "Test Data - 10x_pbmc",
+    {
+        "tenx_mtx_path": LatchFile('s3://latch-public/test-data/4751/emptydrops/data/matrix.mtx'),
+        "out_dir": LatchDir('latch:///'),
+        "lower": 100,
+        "ignore": 0,
+    },
 )
